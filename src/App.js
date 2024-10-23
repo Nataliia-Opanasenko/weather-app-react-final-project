2f9cb59b080fcf850ac5b2697918ef092daa33a1
Nataliia-Opanasenko <146832931+Nataliia-Opanasenko@users.noreply.github.com>
2024-10-23 22:07:13 +0200

    Added Github Url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"> Weather App
+    <div className="App">
+      {" "}
+      Weather App
+      <footer>
+        This project was coded by
+        <a
+          href="https://nataliia-opanasenko-portfolio.netlify.app/"
+          target="_blank"
+        >
+          {" "}
+          Nataliia Opanasenko
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Nataliia-Opanasenko/weather-app-react-final-project"
+          target="_blank_"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
