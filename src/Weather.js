1abede7861da85946b17cc13fe092f7c210d8a3a
Nataliia-Opanasenko <146832931+Nataliia-Opanasenko@users.noreply.github.com>
2024-10-25 13:48:44 +0200

    added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
                 type="search"
                 placeholder="Enter a city..."
                 className="form-control"
+                autoFocus="on"
               />
             </div>
             <div className="col-3">
-              <input type="submit" value="Seatch" className="btn btn-primary" />
+              <input
+                type="submit"
+                value="Seatch"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
         </form>
@@ -23,13 +28,17 @@ export default function Weather() {
           <li>Monday 7:00</li>
           <li>Mostly Cloudy</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3">
           <div className="col-6">
-            <img
-              src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
-              alt="Mostly Cloudy"
-            />
-            6°C
+            <div className="clearfix">
+              <img
+                src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                alt="Mostly Cloudy"
+              />
+
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
           </div>
           <div className="col-6">
             <ul>
