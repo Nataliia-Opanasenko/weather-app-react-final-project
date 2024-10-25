94836682e0ac65a543e2663b4cc7e16331828440
Nataliia-Opanasenko <146832931+Nataliia-Opanasenko@users.noreply.github.com>
2024-10-25 11:55:44 +0200

    Added the HTML structure

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {" "}
-      <h1>Weather App</h1>
-      <footer>
-       
-        This project was coded by
-        <a
-          href="https://nataliia-opanasenko-portfolio.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Nataliia Opanasenko
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Nataliia-Opanasenko/weather-app-react-final-project"
-          target="_blank_"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by
+          <a
+            href="https://nataliia-opanasenko-portfolio.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Nataliia Opanasenko
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Nataliia-Opanasenko/weather-app-react-final-project"
+            target="_blank_"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
